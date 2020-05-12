cf474ec83712a1031c187b77519f9d76c7d3e619
Azhar441 <64615389+Azhar441@users.noreply.github.com>
2020-05-12 19:22:28 +0800

    Create Jobsheet 7 C.c
    Mengisi elemen array dengan perulangan

---
+++ Jobsheet 7 C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+	printf ("\nNAMA : AZHAR");
+    printf ("\nNIM  : F1B019033");
+ 
+	int x[10],i;
+	for(i=0;i<10;i++)
+{
+	printf("\nMasukkan nilai %i : ",i+1) ;
+	scanf("%i",&x[i]) ;
+	}
+	return 0;
+}
