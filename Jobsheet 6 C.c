57150e0f9bebec4c27da4874291ccc0dae38d0ae
Azhar441 <64615389+Azhar441@users.noreply.github.com>
2020-05-12 19:19:13 +0800

    Create Jobsheet 6 C.c
    Transpose 2 matrix

---
+++ Jobsheet 6 C.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main() { 
+int matrix[3][3]; 
+int transpose[3][3]; 
+int i,j; 
+printf ("\nNAMA : AZHAR");
+printf ("\nNIM  : F1B019033");
+
+for(i=0;i<3;i++)
+{ for(j=0;j<3;j++)
+{ printf("\nMasukan Nilai Matriks [%d][%d] : ", i+1, j+1);
+scanf("%d",&matrix[i][j]);
+}
+}
+
+for (i=0;i<3;i++) { 
+	for (j=0;j<3;j++) {
+transpose[j][i] = matrix[i][j]; } } 
+
+printf("\nMatrik A [3x3]\n"); 
+for (i=0;i<3;i++) { 
+for (j=0;j<3;j++) { 
+printf("%5d",matrix[i][j]); 
+} printf("\n"); 
+} 
+ 
+printf("\nMatrik A Transpose [3x3]\n"); 
+for (i=0; i<3; i++) { 
+for (j=0; j<3; j++) { 
+printf("%5d",transpose[i][j]); 
+} 
+printf("\n"); 
+} 
+}
