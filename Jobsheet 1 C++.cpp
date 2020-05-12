f6c3e2780e5c72532db627da98591425636b852e
Azhar441 <64615389+Azhar441@users.noreply.github.com>
2020-05-12 19:42:39 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main (){
+string my_array[5]={"F1B019033","F1B019059","F1B019057"};
+	
+	cout<<"nim mahasiswa ke- 1 = "<<my_array[0]<<"\n";
+	cout<<"nim mahasiswa ke- 2 = "<<my_array[1]<<"\n";
+	cout<<"nim mahasiswa ke- 3 = "<<my_array[2]<<"\n";
+	cout<<"nim mahasiswa ke- 4 = "<<my_array[3]<<"\n";
+	
+}
