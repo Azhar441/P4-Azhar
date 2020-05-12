961d2733ed09012a1c68e4059b51b27202a14000
Azhar441 <64615389+Azhar441@users.noreply.github.com>
2020-05-12 19:44:56 +0800

    Create Jobsheet 2 C++.cpp
    Mengisi dan menampilkan array dinamis

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	cout<<"\tNAMA     : AZHAR"<<endl;
+	cout<<"\tKELOMPOK : 12"<<endl;
+	
+    int x [5];
+    cout<<"nim mahasiswa 1 : ";
+	cin>>x[0];
+	cout<<"nim mahasiswa 2 : ";
+	cin>>x[1];
+	cout<<"nim mahasiswa 3 : ";
+    cin>>x[2];
+	cout<<"nim mahasiswa 4 : ";
+	cin>>x[3];
+    cout<<"nim mahasiswa 5 : ";
+	cin>>x[4];
+
+	cout<<endl;
+	cout<<"nim ke_1 = "<<x[0]<<endl;
+	cout<<"nim ke_2 = "<<x[1]<<endl;
+	cout<<"nim ke_3 = "<<x[2]<<endl;
+	cout<<"nim ke_4 = "<<x[3]<<endl;
+	cout<<"nim ke_5 = "<<x[4]<<endl;
+	
+	return 0;
+}
