ca2435880cb06c50fd3579f30ee2ef86cfd31422
Azhar441 <64615389+Azhar441@users.noreply.github.com>
2020-05-12 20:06:23 +0800

    Create Jobsheet 7 C++.cpp
    Mengisi elemen array dengan perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	cout<<"\tNAMA : AZHAR"<<endl;
+    cout<<"\tNIM  : F1B019033"<<endl;
+
+	int x[10],i;
+	for(i=0;i<10;i++)
+	{
+	cout<<"nilai"<<i+1<<" : ";
+	cin>>x[i];
+}
+return 0;
+}
